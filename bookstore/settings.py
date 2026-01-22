c7dd5880a9e0b995d37e6d71b606d8d4476dfe29
Abdulrhman alzarei <alzareiabdulrhman@gmail.com>
2026-01-23 00:43:39 +0300

    Move images to static and update templates

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -20,9 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 SECRET_KEY = 'SECRET_KEY'
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['abdulrhaman.pythonanywhere.com']
 
 
 # Application definition
